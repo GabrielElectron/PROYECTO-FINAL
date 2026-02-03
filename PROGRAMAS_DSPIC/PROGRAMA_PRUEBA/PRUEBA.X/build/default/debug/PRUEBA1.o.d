8d9393d19ee4fb94026638f26219021e1ce01fdd
Gabriel Wint <87378647+GabrielElectron@users.noreply.github.com>
2026-02-03 17:37:56 -0300

    Delete PROGRAMAS_DSPIC directory

--- PROGRAMAS_DSPIC/PROGRAMA_PRUEBA/PRUEBA.X/build/default/debug/PRUEBA1.o.d
+++
@@ -1,7 +0,0 @@
-build/default/debug/PRUEBA1.o: PRUEBA1.c \
- mcc_generated_files/system/system.h \
- mcc_generated_files/system/system_types.h
-
-mcc_generated_files/system/system.h:
-
-mcc_generated_files/system/system_types.h:
--- PROGRAMAS_DSPIC/PROGRAMA_PRUEBA/PRUEBA.X/build/default/production/PRUEBA1.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/PRUEBA1.o: PRUEBA1.c \
- mcc_generated_files/system/system.h \
- mcc_generated_files/system/system_types.h
-
-mcc_generated_files/system/system.h:
-
-mcc_generated_files/system/system_types.h:
